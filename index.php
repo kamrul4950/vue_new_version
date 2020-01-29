8335725f1931c536444ea019991c871914f198c2
kamrul4950 <kamrulasad49@gmail.com>
2020-01-30 01:13:59 +0600

    check object attribute

--- index.php
+++ index.php
@@ -21,13 +21,19 @@
             </span>
         
         
-            <h2>Fabarate lanaguages is</h2>
+            <h2>Fabarate lanaguages is ({{languages.length}})</h2>
             <ol>
-                <li v-for="language in numbers">
+                <li v-for="language in languages">
                     {{language}}
                 </li>
             </ol>
-       
+            <br>
+            <h2>Student Details</h2>
+            <p>Name: {{student.name}}</p>
+            <p>Age : {{student.age}}</p>
+            
+            <hr>
+
             
         </div>
         
@@ -44,7 +50,25 @@
                 about:"Hei I am Md Kamrul Asad </br> Department is <b> CSE</b>",
                 phone_no:"01823-335388",
                 gender:true,
-                numbers:100
+                languages:[
+                    "C",
+                    "C++",
+                    "Java",
+                    "PHP",
+                    "C#"
+
+
+                ],
+                books:{
+                    name:"Java Programming",
+                    price: 250
+                },
+                student:{
+                    name:"Md Kamrul Asad",
+                    age: 25,
+                    mobile:"01823-335388",
+                    deprt:"CSE"
+                }
             }
            
         })
