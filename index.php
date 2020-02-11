5e64f6fb2d3eba899a67af4a9130e5e80af71a7e
kamrul4950 <kamrulasad49@gmail.com>
2020-02-12 05:26:56 +0600

    Vue js Daily note book system

--- index.php
+++ index.php
@@ -9,43 +9,51 @@
 </head>
 <body>
         <div id="app">
-        <div class="container">
-          <h2>Calclutor Project Vue Js</h2>
+          <div class="container">
             <div class="card">
-              <div class="card-body">
-                  
-                  <div class="alert alert-danger" v-if="hasError">
-                    Error : {{error}}
-                  </div>
-                  <div class="alert alert-success" v-else>
-                    Result : {{result}}
-                  </div>
+              <h2>Note App Vue Js</h2>
+              <div class="card card-body" v-if="errorText.length>0">
+                <div class="alert alert-danger" >
+                  {{errorText}}
+                </div>
+              </div>
+              <div class="card card-body" v-if="successText.length>0">
+                <div class="alert alert-success">
+                  {{successText}}
+                </div>
+              </div>
+              <div class="card card-body">
                   <div class="row">
                     <div class="col-md-6">
-                      <input type="text" v-model="frist_num" class="form-control" v-on:keyup="numberChick">
+                      <p>Title  {{ title }}</p>
+                      <input type="text" class="form-control" v-model="title">
                     </div>
                     <div class="col-md-6">
-                      <input type="text" v-model="second_num" class="form-control" v-on:keyup="numberChick">
+                      <p>Description  {{ description }}</p>
+                      <textarea class="form-control" cols="30" rows="5" v-model="description"></textarea>
                     </div>
                   </div>
-                  <div class="row mt-3">
-                    <div class="col-md-3">
-                      <button type="button" class="btn btn-success btn-block" v-on:click="sumationFun">+</button>
-                    </div>
-                    <div class="col-md-3">
-                      <button type="button" class="btn btn-success btn-block" v-on:click="subtractionFun">-</button>
-                    </div>
-                    <div class="col-md-3">
-                      <button type="button" class="btn btn-success btn-block" v-on:click="divisionFun">/</button>
-                    </div>
-                    <div class="col-md-3">
-                      <button type="button" class="btn btn-success btn-block" v-on:click="multiplactionFun">*</button>
+                  <div class="row">
+                    <div class="col-md-12">
+                      <button type="button" class="btn btn-success btn-block mt-2" v-on:click="addFun" >Add Note</button>
                     </div>
                   </div>
               </div>
+              <div class="card card-body mt-3 p-3" v-for="(note , index) in notes">
+                <div class="row">
+                  <div class="col-md-12">
+                    <p class="btn btn-danger" v-on:click="deleteNote(index)">X</p>
+                     <h4>{{note.title}}</h4>
+                    <p>{{note.description}}</p>
+                  </div>
+                </div>
+                
+               
+              </div>
             </div>
-        </div>
-          
+              
+            
+          </div>
         </div>
         
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
--- js/main.js
+++ js/main.js
@@ -1,54 +1,48 @@
-var app =new Vue({
-    el:"#app",
+var app = new Vue({
+    el:'#app',
     data:{
-        frist_num:"",
-        second_num:"",
-        result:"",
-        hasError:false,
-        error:""
+        title:'',
+        description:'',
+        notes:[],
+        errorText:'',
+        successText:''
+    },
+    mounted(){
+        this.notes.push(
+            {
+                'title':'1st title new',
+                'description':'This is demo description'
+            },
+            {
+                'title':'2nd title note',
+                'description':'This is 2nd description demo text'
+            }
+        )
     },
     methods:{
-        sumationFun(){
-            
-           var nam1 = Number(this.frist_num) ;
-           var nam2 = Number(this.second_num) ;
-           var result =nam1+nam2;
-           this.result=result;
+        addFun(){
+            let title=this.title;
+            let description=this.description;
+            if(title.length > 0 && description.length >0){
+                let notAdd ={
+                    'title':title,
+                    'description':description
+                }
+                this.notes.push(notAdd);
+                this.title="",
+                this.description="",
+                this.successText="Note Added",
+                this.errorText=""
+            }else{
+                this.successText="",
+                this.errorText="Please fill the input field.."
+                return;
+            }
         },
-        subtractionFun(){
-            
-            var nam1 = Number(this.frist_num);
-            var nam2 = Number(this.second_num);
-            var result = nam1-nam2;
-            this.result=result;
-        },
-        divisionFun(){
-            
-
-            var nam1=Number(this.frist_num);
-            var nam2=Number(this.second_num);
-            var result =nam1/nam2;
-            this.result=result;
-        },
-        multiplactionFun(){
-           
-
-            var nam1 = Number(this.frist_num);
-            var nam2 = Number(this.second_num);
-            var result =nam1*nam2;
-            this.result=result;
-        },
-        numberChick(){
-            if(isNaN(this.frist_num)){
-                            this.hasError=true;
-                            this.error="Please enter valide number..";
-                        }else if(isNaN(this.second_num)){
-                            this.hasError=true;
-                            this.error="Please enter valide number";
-                        }else{
-                            this.hasError=false;
-                            this.error="";
-                        }
+        deleteNote(noteIndex){
+            this.notes.splice(noteIndex,1);
+            this.successText="Note delete success..",
+            this.errorText=""
         }
     }
-})
+});
